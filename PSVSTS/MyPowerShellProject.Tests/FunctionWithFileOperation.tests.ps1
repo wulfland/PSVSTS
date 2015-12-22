2db1d9369a62f136cbf29bb68ac259a6d15b7afb
Michael Kaufmann <mkaufmann@ApplNext.onmicrosoft.com>
2015-12-22 19:48:30 +0100

    Added more tests and functions.

---
+++ PSVSTS/MyPowerShellProject.Tests/FunctionWithFileOperation.tests.ps1
@@ -0,0 +1,33 @@
+﻿$project = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace(".Tests", "")
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".tests.", ".")
+. "$project\$sut"
+
+Describe "FunctionWithFileOperation" {
+	Context "Given the file exists, when the config is valid" {
+		
+		Setup -File config.xml -Content "<App Id='42' />"
+
+		It "retuns the id of the app" {
+
+			FunctionWithFileOperation -Path $TestDrive\config.xml | Should Be 42
+		}
+	}
+
+	Context "Given the file exists, when the config file is invalid" {
+		
+		Setup -File config.xml -Content "<App />"
+
+		It "throws 'The config file is invalid.'" {
+
+			{ FunctionWithFileOperation -Path $TestDrive\config.xml } | Should Throw "The config file is invalid."
+		}
+	}
+
+	Context "Given the file does not exist" {
+
+		It "throws 'The config file is invalid.'" {
+
+			{ FunctionWithFileOperation -Path invalidfile.xml } | Should Throw "The config file does not exist."
+		}
+	}
+}
---
+++ PSVSTS/MyPowerShellProject/FunctionWithFileOperation.ps1
@@ -0,0 +1,28 @@
+<#
+.Synopsis
+   FunctionWithFileOperation
+.DESCRIPTION
+   Gets the ID of a XML file
+.EXAMPLE
+   FunctionWithFileOperation -Path .\somefile.txt
+#>
+function FunctionWithFileOperation
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+		[Parameter(Mandatory=$true, Position=0)]
+		[string]$Path
+	)
+
+	if (-not (Test-Path $Path)) { throw "The config file does not exist." }
+
+	[xml]$xml = Get-Content $Path
+
+	$id = $xml.App.Id
+
+	if (-not $id) { throw "The config file is invalid." }
+
+	return [int]$id
+}

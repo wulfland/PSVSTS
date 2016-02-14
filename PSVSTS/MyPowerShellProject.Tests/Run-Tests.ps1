288901b16f7fd74d2ec2a16d394a482808c73e24
Michael Kaufmann <mkaufmann@ApplNext.onmicrosoft.com>
2016-02-14 10:43:52 +0100

    ...

--- PSVSTS/MyPowerShellProject.Tests/Run-Tests.ps1
+++ PSVSTS/MyPowerShellProject.Tests/Run-Tests.ps1
@@ -25,17 +25,18 @@ $outputFile = Join-Path $SourceDir "TEST-pester.xml"
 $result = Invoke-Pester -Path $SourceDir -CodeCoverage "$SourceDir\PSVSTS\MyPowerShellProject\*.ps1" -PassThru -OutputFile $outputFile -OutputFormat NUnitXml -EnableExit
 
 [xml]$template = "<?xml version='1.0' encoding='utf-8'?>
+<!DOCTYPE coverage SYSTEM 'http://cobertura.sourceforge.net/xml/coverage-04.dtd'>
 <coverage branches-covered='' branches-valid='' branch-rate='' complexity='' line-rate='' lines-covered='' lines-valid='' timestamp='' version=''>
 	<packages></packages>
 </coverage>"
 
 $linesCovered = $result.CodeCoverage.NumberOfCommandsExecuted - $result.CodeCoverage.NumberOfCommandsMissed
 
-$template.coverage.timestamp = "$(Get-Date)"
-$template.coverage.version = "1.0.0.0"
-$template.coverage.'lines-covered' = "$linesCovered"
-$template.coverage.'lines-valid' = "$($result.CodeCoverage.NumberOfCommandsAnalyzed)"
-$template.coverage.'line-rate' = "$($linesCovered / $result.CodeCoverage.NumberOfCommandsAnalyzed)"
+#$template.coverage.timestamp = "$(Get-Date)"
+#$template.coverage.version = "1.0.0.0"
+#$template.coverage.'lines-covered' = "$linesCovered"
+#$template.coverage.'lines-valid' = "$($result.CodeCoverage.NumberOfCommandsAnalyzed)"
+#$template.coverage.'line-rate' = "$($linesCovered / $result.CodeCoverage.NumberOfCommandsAnalyzed)"
 
 $template | Out-File (Join-Path $SourceDir "coverage.xml")
 
